27a72d63aa08828d8863bedf27c37595e526859e
Shivesh kumar <shivesh.shivendra@gmail.com>
2024-11-05 18:11:43 +0530

    Fix: Update router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <HomePage />
-    </div>
+    </Router>
   );
 }
 
-export default App;
